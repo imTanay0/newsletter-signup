c73d095e1ef7411d4a532a5fca0aebd7dfec1789
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-01-10 01:33:48 +0530

    small fixes

--- app.js
+++ app.js
@@ -1,74 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const https = require('https');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    // res.send("Hello");
-    res.sendFile(__dirname + "\\signup.html");
+  // res.send("Hello");
+  res.sendFile(__dirname + "\\signup.html");
 });
 
 app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-    const firstName =req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName
-                }
-            }
-        ]
-    };
+  const jsonData = JSON.stringify(data);
 
-    const jsonData = JSON.stringify(data);
+  const url = "https://us12.api.mailchimp.com/3.0/lists/b3b0885822";
 
-    const url = "https://us12.api.mailchimp.com/3.0/lists/b3b0885822";
+  const options = {
+    method: "POST",
+    auth: "catiger:6f789c165cd552d61273c48c75a8f392-us12",
+  };
 
-    const options = {
-        method: "POST",
-        auth: "catiger:6f789c165cd552d61273c48c75a8f392-us12"
-    };
+  const request = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "\\success.html");
+    } else {
+      res.sendFile(__dirname + "\\failure.html");
+    }
 
-    const request = https.request(url, options, (response) => {
-
-        if(response.statusCode === 200) {
-            res.sendFile(__dirname + "\\success.html");
-        } else {
-            res.sendFile(__dirname + "\\failure.html");
-        }
-
-
-        response.on("data", (data) => {
-            console.log(JSON.parse(data));
-        });
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
     });
+  });
 
-    request.write(jsonData);
-    request.end();
-
+  request.write(jsonData);
+  request.end();
 });
 
 app.post("/failure", (req, res) => {
-    res.redirect("/");
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("server is running at port 3000.");
+  console.log("server is running at port 3000.");
 });
-
-
 
 // API Key
 // 6f789c165cd552d61273c48c75a8f392-us12

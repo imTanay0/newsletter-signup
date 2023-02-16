e9f977c3cb4eb078e52a1bea82e89efd6b416b6d
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-02-16 13:00:17 +0530

    update app.js

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env || 3000, () => {
   console.log("server is running at port 3000.");
 });
